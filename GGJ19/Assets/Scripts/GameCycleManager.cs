29311ab192354f89c9ef2c796564482b414560b4
rasca0027 <rasca0027@gmail.com>
2019-01-26 11:01:57 -0500

    first commit

---
+++ GGJ19/Assets/Scripts/GameCycleManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCycleManager : MonoBehaviour {
+
+	int weekNum; // 1 ~ 52
+	int actionPts;
+	int maxActionPts;
+	Player player;
+	GameState state;
+
+    public enum GameState 
+	{
+		Weekday,
+		Weekend,
+		End
+	}
+
+	// Use this for initialization
+	void Start () {
+		player = new Player("kelly");
+		weekNum = 1;
+		maxActionPts = 12;
+		actionPts = maxActionPts;
+		state = GameState.Weekday;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (state == GameState.Weekday)
+		{
+
+		}
+		else if (state == GameState.Weekend)
+		{
+
+		}
+		else // End, evaluate visa
+		{
+
+		}	
+	}
+}
---
+++ GGJ19/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player 
+{
+	public string name;
+	public string occupation;
+	public string nativeLanguage;
+	
+	// stats
+	public float technicalSkills;
+	public float stressHappyLevel;
+	public float languageAbility;
+	public float networkCircle;
+
+	public Player(string playerName)
+	{
+		name = playerName;
+		occupation = "Game Dev";
+		nativeLanguage = "Mandarin";
+		technicalSkills = 0.5f;
+		stressHappyLevel = 0.5f;
+		languageAbility = 0.5f;
+		networkCircle = 0.5f;
+	}
+
+
+	public void ImproveTechnicalSkills()
+	{
+
+	}
+
+	public void TalkToFamily()
+	{
+
+	}
+
+	public void ExpandNetwork()
+	{
+
+	}
+
+}
